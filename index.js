4644c11cc63e250ec5a8d64fc71ea1c9889dee2e
floatflower <floatflower1029@gmail.com>
2022-06-28 09:34:03 +0000

    Update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.static('public', {
   index: 'index.html'
 }));
 app.get('/capital/:id', require('./routes/capital'));
+app.get('/avatar/capital/:id', require('./routes/capital'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
